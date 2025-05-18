80b2c0af6d67d0ca0e8585f619b5a16a5109211a
Mathew-Rym <rymmathew@gmail.com>
2025-05-19 00:17:01 +0300

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        sorted_word = "".join(sorted([letter for letter in self.word]))
+        matches = []
+        for word in word_list:
+            if "".join(sorted([letter for letter in word])) == sorted_word:
+                matches.append(word)
+        return matches
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
